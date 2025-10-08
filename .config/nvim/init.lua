54bb2f3f6e92b1976b06567f48bb811de0a19fcd
Riley Spires <rjs243@pm.me>
2025-10-07 23:51:02 -0400

    Fix lsp keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -148,7 +148,9 @@ cmp.setup.cmdline(":", {
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local function on_attach(_, bufnr)
+-- local function on_attach(_, bufnr)
+local function on_attach(e)
+    local bufnr = e.buf
 	local opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
@@ -166,21 +168,17 @@ require("mason-lspconfig").setup {
 	function(server_name)
 		require("lspconfig")[server_name].setup {
 			capabilities = capabilities,
-			on_attach = on_attach,
 		}
 	end,
 }
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = on_attach
+})
 
 -- manual setup of clangd due to arm64 clangd not supported by mason
 require("lspconfig").clangd.setup {
 	capabilities = capabilities,
 	on_attach = on_attach
-}
-
--- manual setup of ols due to arm64 clangd not supported by mason
-require("lspconfig").ols.setup {
-    capabilities = capabilities,
-    on_attach = on_attach
 }
 
 require("autoclose").setup()
