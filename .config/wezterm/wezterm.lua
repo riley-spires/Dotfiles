bb49e7e3158081df8c1b6fa952832addce20534f
Riley Spires <rjs243@pm.me>
2025-10-09 12:53:34 -0400

    Add keybinds to wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -17,5 +18,27 @@ config.enable_scroll_bar = true
 
 -- Set Framerate
 config.max_fps = 240
+
+-- Set keybinds
+config.keys = {
+    {
+        key = 't',
+        mods = 'CTRL',
+        action = act.SpawnTab('CurrentPaneDomain')
+    },
+    {
+        key = 'w',
+        mods = 'CTRL',
+        action = act.CloseCurrentTab{ confirm = false }
+    }
+}
+for i = 1, 9 do
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = 'CTRL',
+        action = act.ActivateTab(i-1)
+    })
+end
+
 
 return config
